7c14d4e78f16962d9964b7505b80052180c5b8b1
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2025-09-27 22:09:52 +0300

    init tg bot

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,35 @@
-import express from "express"
+const TelegramBot = require('node-telegram-bot-api');
 
-const PORT = 5000;
+const token = '7695137489:AAFGRuN9ECFPf9VhO72Bj_LhmCOBiL1Zbf8';
+const webAppUrl = "https://ya.ru"
+const bot = new TelegramBot(token, { polling: true });
 
-const app = express()
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    if (text === "/start") {
+        await bot.sendMessage(chatId, "Ниже кнопка", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "Кнопка", web_app: { url: webAppUrl } },]
+                ]
+            }
+        })
+        await bot.sendMessage(chatId, "Делай", {
+            reply_markup: {
+                keyboard: [
+                    [{ text: "Кнопка вау", web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+    }
+});
+// const PORT = 5000;
 
-app.get("/")
+// const app = express()
 
-app.listen(PORT, () => { console.log("App started") })
+// app.get("/", (req, res) => {
+//     res.status(200).json("200 return")
+// })
+
+// app.listen(PORT, () => { console.log("App started") })
