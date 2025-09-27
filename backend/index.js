f01a0e9a1c56e138ba60c9455ad926f1eb9b1dbd
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2025-09-28 00:12:49 +0300

    init backend

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api');
+import express from "express"
 
-const token = '7695137489:AAFGRuN9ECFPf9VhO72Bj_LhmCOBiL1Zbf8';
-const webAppUrl = "https://miniapp.plexmad.ru/"
-const bot = new TelegramBot(token, { polling: true });
+const PORT = 5000;
 
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-    if (text === "/start") {
-        await bot.sendMessage(chatId, "Ниже кнопка", {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: "Кнопка", web_app: { url: webAppUrl } },]
-                ]
-            }
-        })
-        await bot.sendMessage(chatId, "Делай", {
-            reply_markup: {
-                keyboard: [
-                    [{ text: "Кнопка вау", web_app: { url: webAppUrl } }]
-                ]
-            }
-        })
-    }
-});
-// const PORT = 5000;
+const app = express()
 
-// const app = express()
+app.get("/")
 
-// app.get("/", (req, res) => {
-//     res.status(200).json("200 return")
-// })
-
-// app.listen(PORT, () => { console.log("App started") })
+app.listen(PORT, () => { console.log("App started") })
---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <h1 className='text-rose-300'>Список игр</h1>
+        
+    );
+};
+
+export default MainPage;
