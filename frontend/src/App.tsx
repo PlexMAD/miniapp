6b8adfba3f7da3f789c455693f3fae7b2cc9a99b
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2025-09-27 22:51:37 +0300

    move

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import './App.css'
-
+const tg = window.Telegram.WebApp;
 function App() {
-
+  useEffect(() => {
+    tg.ready()
+  }, []);
+  const onClose = () => {
+    tg.close()
+  }
   return (
     <>
       <div>sdjnaujnids</div>
+      <button onClick={onClose}>asddas</button>
     </>
   )
 }
