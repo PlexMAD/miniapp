38fc91155ed60328021ca13a156c19d44a6bd47e
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2025-09-28 00:37:48 +0300

    init scss and backend unit folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import './App.css'
+import MainPage from './pages/MainPage/MainPage';
+import DefaultLayout from './layouts/DefaultLayout';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const tg = window.Telegram.WebApp;
@@ -14,6 +16,9 @@ function App() {
     <>
       <div className='text-3xl font-bold underline'>sdjnaujnids</div>
       <button onClick={onClose}>asddas</button>
+      <DefaultLayout>
+        <MainPage />
+      </DefaultLayout>
     </>
   )
 }
---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./Mainpage.module.scss"
+
+export const MainPage = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Список игр</h1>
+    </div>
+  );
+};
+
+export default MainPage;
